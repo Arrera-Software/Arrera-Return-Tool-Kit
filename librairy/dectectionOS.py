5be49c7d83d8bad1b9897a6811ec52b5533bc315
Baptiste P <baptiste.pauchet13@gmail.com>
2024-10-17 16:50:29 +0200

    Ajout de la librairy decectionOS.py

---
+++ librairy/dectectionOS.py
@@ -0,0 +1,18 @@
+import platform
+
+
+class OS:
+    def __init__(self):
+        self.__os = platform.system()
+
+    def osWindows(self):
+        if self.__os == "Windows":
+            return True
+        else:
+            return False
+
+    def osLinux(self):
+        if self.__os == "Linux":
+            return True
+        else:
+            return False
