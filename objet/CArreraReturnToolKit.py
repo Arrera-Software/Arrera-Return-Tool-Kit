77a21b75c11754b7ce3d37c787cc9df6ff67dfab
Baptiste P <baptiste.pauchet13@gmail.com>
2024-10-17 21:03:00 +0200

    Ajout de nouvelle entrée dans le json

--- objet/CArreraReturnToolKit.py
+++ objet/CArreraReturnToolKit.py
@@ -16,13 +16,14 @@ class CArreraReturnToolKit :
         self.__textcolor = configFile.lectureJSON("textColor")
         self.__styleTextTitle = ("Arial",25)
         self.__styleText = ("Arial", 15)
+        self.__txtBTN = [configFile.lectureJSON("btn1Text"),configFile.lectureJSON("btn3Text"),
+                         configFile.lectureJSON("btn2Text"),configFile.lectureJSON("btn4Text")]
 
     def active(self):
         # Creation de la fenetre
         win = Toplevel()
         win.title(self.__name)
         if (OS().osLinux() == True):
-
             win.maxsize(500, 635)
             win.minsize(500, 635)
         else:
@@ -38,7 +39,22 @@ class CArreraReturnToolKit :
                            font=self.__styleTextTitle,fg=self.__textcolor)
         btnQuit = Button(win,text="Quitter",bg=self.__color,
                          fg=self.__textcolor,font=self.__styleText)
+        #BTN
+        btn1 = Button(frameBTN,text=self.__txtBTN[0],font=self.__styleText,
+                      bg=self.__color,fg=self.__textcolor,wraplength=100)
+        btn2 = Button(frameBTN, text=self.__txtBTN[1], font=self.__styleText,
+                      bg=self.__color, fg=self.__textcolor,wraplength=100)
+        btn3 = Button(frameBTN, text=self.__txtBTN[2], font=self.__styleText,
+                      bg=self.__color, fg=self.__textcolor,wraplength=100)
+        btn4 = Button(frameBTN, text=self.__txtBTN[3], font=self.__styleText,
+                      bg=self.__color, fg=self.__textcolor,wraplength=100)
         # Affichage
         frameBTN.place(relx=0.5, rely=0.5, anchor="center")
+
+        btn1.place(relx=0.0, rely=0.0, anchor='nw')
+        btn2.place(relx=1.0, rely=0.0, anchor='ne')
+        btn3.place(relx=0, rely=1, anchor='sw')
+        btn4.place(relx=1, rely=1, anchor='se')
+
         labelTitle.pack()
         btnQuit.pack(side="bottom")
